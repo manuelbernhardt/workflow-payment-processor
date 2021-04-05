70d466b5f098e3c2a60239e2e7887f742a50a53f
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2021-04-05 10:43:45 +0200

    Throughput benchmark

--- app/src/jmh/java/io/bernhardt/workflow/payment/PaymentHandlingWorkflowBenchmark.java
+++ app/src/jmh/java/io/bernhardt/workflow/payment/PaymentHandlingWorkflowBenchmark.java
@@ -16,7 +16,18 @@ public class PaymentHandlingWorkflowBenchmark {
     @Benchmark
     @BenchmarkMode(Mode.AverageTime)
     @OutputTimeUnit(TimeUnit.MILLISECONDS)
-    public PaymentResult runWorkflow(WorkflowSetup setup) {
+    public PaymentResult runWorkflowAvgExecutionTime(WorkflowSetup setup) {
+        // Start a workflow execution. Usually this is done from another program.
+        // Uses task queue from the GreetingWorkflow @WorkflowMethod annotation.
+        PaymentHandlingWorkflow workflow = setup.client.newWorkflowStub(PaymentHandlingWorkflow.class, WorkflowOptions.newBuilder().setTaskQueue(TASK_QUEUE).build());
+        // Execute a workflow waiting for it to complete.
+        return workflow.handlePayment(new OrderId(UUID.randomUUID().toString()), setup.amountToSpend, setup.merchantId, setup.userId);
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.Throughput)
+    @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    public PaymentResult runWorkflowThroughput(WorkflowSetup setup) {
         // Start a workflow execution. Usually this is done from another program.
         // Uses task queue from the GreetingWorkflow @WorkflowMethod annotation.
         PaymentHandlingWorkflow workflow = setup.client.newWorkflowStub(PaymentHandlingWorkflow.class, WorkflowOptions.newBuilder().setTaskQueue(TASK_QUEUE).build());
